3793d811081b525ac6cae22862067ca65de49b9d
lassieh <lassi.hameen-anttila@metropolia.fi>
2019-12-05 10:05:04 +0200

    burning out

--- app.js
+++ app.js
@@ -2,9 +2,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+const catRoute = require('./routes/catRouter');
+const userRouter = require('./routes/userRouter');
 
-app.get('/', (req, res) => {
-  res.send('Home');
-});
+app.use(cors());
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('uploads'));
+
+app.use('/cat', catRoute);
+
+app.use('/user', userRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
